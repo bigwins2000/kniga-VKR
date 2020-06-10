42c56977cf390214f47bba748c4daaee2644e08a
bigwins2000 <47929505+bigwins2000@users.noreply.github.com>
2020-06-10 23:42:23 +0400

    123

--- Unit2.pas
+++ Unit2.pas
@@ -4,15 +4,20 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.OleCtrls, SHDocVw,
+  Vcl.ComCtrls, Vcl.ExtCtrls;
 
 type
   TForm2 = class(TForm)
     ScrollBox1: TScrollBox;
     Memo1: TMemo;
+    Image1: TImage;
+    RichEdit1: TRichEdit;
+    WebBrowser1: TWebBrowser;
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
     procedure FormResize(Sender: TObject);
     procedure FormShow(Sender: TObject);
+    procedure ScrollBox1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -45,6 +50,11 @@ procedure TForm2.FormShow(Sender: TObject);
 begin
 
 Memo1.Lines.LoadFromFile('BD/1.1.txt');
+end;
+
+procedure TForm2.ScrollBox1Click(Sender: TObject);
+begin
+Image1.Canvas.TextOut(1,1,'rtyui');
 end;
 
 end.
